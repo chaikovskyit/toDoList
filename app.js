b403a77e0d8070d6872228209ac343079f780f89
chaikovskyit <chaikovskyit@gmail.com>
2020-09-23 20:40:12 +0300

    Add html structure and JS script

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const input = document.querySelector('.input')
+const button = document.querySelector('.btn')
+const blockList = document.querySelector('.tasks')
 
+
+function pushElement(text){
+    const newElement = document.createElement('li')
+    const deleteBatton = document.createElement('button')
+
+    deleteBatton.textContent = 'delete'
+    deleteBatton.addEventListener('click', function(e){
+        blockList.removeChild(newElement)
+    })
+
+    newElement.textContent = text
+    newElement.appendChild(deleteBatton)
+    blockList.appendChild(newElement)
+}
+
+button.addEventListener('click', function(e){
+    pushElement(input.value)
+    input.value = ''
+})
