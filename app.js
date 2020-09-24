c76be3e391ffa828cf81dafea6908bdbf6a1b826
chaikovskyit <chaikovskyit@gmail.com>
2020-09-24 17:35:39 +0300

    add alert, if input value lengt empty

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const input = document.querySelector('.input')
 const button = document.querySelector('.btn')
 const blockList = document.querySelector('.tasks')
 
-
 function pushElement(text){
     const newElement = document.createElement('li')
     const deleteBatton = document.createElement('button')
@@ -18,6 +17,11 @@ function pushElement(text){
 }
 
 button.addEventListener('click', function(e){
-    pushElement(input.value)
-    input.value = ''
+    if(input.value.length >= 1 ){
+        pushElement(input.value)
+        input.value = ''
+    } else {
+        alert('Input field cannot be empty')
+    }
+    
 })
